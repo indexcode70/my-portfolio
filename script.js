d8a4f325425e4e4a4248af50ceb400a7b3394cc8
indexcode70 <259004508+indexcode70@users.noreply.github.com>
2026-03-24 15:09:25 +0900

    使用技術の名称変更とアプリ紹介文の編集

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const myWorks = [
     {
     title: "おみくじ",
     description: "ボタン操作で運勢をランダムに表示するアプリです。javaScriptのMathオブジェクトと配列インデックスの操作を組み合わせたロジックを実装し、基本的な動的コンテンツの生成を学びました。",
-    tech: "HTMl / CSS / javaScript",
+    tech: "HTML5 / CSS3 / JavaScript[ES6+](Math.random / DOM)",
     link:"https://indexcode70.github.io/omikuji-app/"
     },
 
     {
     title: "お天気実況",
     description: "OpenWeatherMap APIを利用し、指定した都市の天候と気温をリアルタイムに取得し表示します。Fetch APIによる非同期処理に加え、天候データに応じて動的生成したCSSアニメーションによる演出など、視覚的な楽しさにこだわりました。",
-    tech: "HTMl / CSS / javaScript",
+    tech: "HTML5 / CSS3 / JavaScript[ES6+](Fetch API / async-await)",
     link: "https://indexcode70.github.io/weather-app/"
     },
 
     {
     title: "お買い物リスト",
-    description: "日々の買い物を管理できるタスク管理アプリです、Web Storage API(LocalStorage)を活用し、ブラウザを閉じてもデータが保持される永続化を実現しました。CSSのcalc関数を用いたレスポンシブなレイアウト設計にも挑戦しています。",
-    tech: "HTML / CSS / javaScript",
+    description: "日々の買い物を管理できるタスク管理アプリです。Web Storage API(LocalStorage)を活用し、ブラウザを閉じてもデータが保持される永続化を実現しました。CSSのcalc関数を用いたレスポンシブなレイアウト設計にも挑戦しています。",
+    tech: "HTML5 / CSS3(calc) / JavaScript[ES6+](LocalStorage / JSON)",
     link: "https://indexcode70.github.io/shopping-memo_app/"   
     },
 
@@ -37,7 +37,7 @@ function displayWorks() {
         // カードの中身を組み立てる
         card.innerHTML = `
         <h3>${work.title}</h3>
-        <p class="tech-stack">使用技術: ${work.tech}</p>
+        <p class="tech-stack">Tech-Stack: ${work.tech}</p>
         <p>${work.description}</p>
         <a href="${work.link}"class="view-button" target="_blank">作品を見る</a>
         `;
